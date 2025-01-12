d29ce91d3323ce286d07d1ac627be1b2b737cb46
master <rexlicky_verdhika_sagatha@teknokrat.ac.id>
2025-01-12 17:47:51 +0700

    sagatha: menambahkan halaman login

--- program/app/login/page.tsx
+++ program/app/login/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import FormLogin from "./components/FormLogin";
+import FormLogin from './components/FormLogin'
 
 const Page = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  const handleOpenLogin = () => setShowLogin(true);
-  const handleCloseLogin = () => setShowLogin(false);
-
   return (
     <>
-      {/* Main Page Content */}
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center z-40">
-        <button className="btn btn-primary" onClick={handleOpenLogin}>
-          Login
-        </button>
-      </div>
-
-      {/* Render Login Form if `showLogin` is true */}
-      {showLogin && <FormLogin handleClose={handleCloseLogin} />}
+      <FormLogin />
     </>
   );
 };

f6a78fd6587fce5b2fdfe7128a495c22b3f68a19
master <rexlicky_verdhika_sagatha@teknokrat.ac.id>
2025-01-12 17:46:53 +0700

    sagatha: menambahkan register page

---
+++ program/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormRegister from './components/FormRegister'
+
+function page() {
+  return (
+    <>
+      <FormRegister />
+    </>
+  );
+}
+
+export default page

c2461ac8a7aa31038f4d29e1964c65a9cbd19f54
master <rexlicky_verdhika_sagatha@teknokrat.ac.id>
2025-01-12 21:02:10 +0700

    sagatha: update tailwind config

---
+++ Program/adlv-app/tailwind.config.ts
@@ -0,0 +1,69 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  darkMode: ["class"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        kiwkiw: "#eaeef1",
+        ...colors,
+      },
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        scroll: {
+          to: { transform: "translate(calc(-50% - 0.5rem))" },
+        },
+      },
+      height: {
+        "90vh": "90vh",
+        "100vh": "100vh",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        custom: "0 0 4px rgba(0,0,0,0.25)",
+      },
+    },
+  },
+  plugins: [
+    require("daisyui"),
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+  ],
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
